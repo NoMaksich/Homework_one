549c6ede91ebb8f49bfd20cfaff4cd70ad0fe9b3
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-22 12:53:24 +0300

    Первое задание выполнено

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int one_number=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int two_number=int.Parse(Console.ReadLine());
+int max;
+if (one_number>two_number)
+    max=one_number;
+else
+    max=two_number;
+Console.WriteLine("Максимальное число: " + max);
+
+
