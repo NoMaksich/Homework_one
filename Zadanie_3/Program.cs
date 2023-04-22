136b6fc7bc71cbb84fd4fb47ce454633c928ec06
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-22 12:53:55 +0300

    Третье задание выполнено

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int one_number=int.Parse(Console.ReadLine());
+if (one_number%2==0)
+    Console.WriteLine("Число "+one_number + " чётное");
+else
+     Console.WriteLine("Число "+one_number + " нечётное");
