e628e894510588a05c36c9f365328b6fc0f5969a
maximmetalnikov <maxummetalnikov@mail.ru>
2023-04-22 12:54:04 +0300

    Четвертое задание выполнено

---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int one_number=int.Parse(Console.ReadLine());
+int count=1;
+while (count<=one_number)
+{
+    if (count%2==0)
+        Console.Write(count+", ");
+    count+=1;
+}
